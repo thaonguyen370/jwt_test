c294bd925684fc1c38222b891947b7f950b0cbf0
thaonguyen370 <nguyenvanthao95@example.com>
2019-02-11 00:14:40 +0700

    Initial commit

---
+++ src/main/java/com/example/jwt/jwt/config/JwtSecurityConfig.java
@@ -0,0 +1,72 @@
+package com.example.jwt.jwt.config;
+
+
+import com.example.jwt.jwt.security.JwtAuthenticationEntryPoint;
+import com.example.jwt.jwt.security.JwtAuthenticationProvider;
+import com.example.jwt.jwt.security.JwtAuthenticationTokenFilter;
+import com.example.jwt.jwt.security.JwtSuccessHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Collections;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+@Configuration
+public class JwtSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Autowired
+    private JwtAuthenticationProvider authenticationProvider;
+    @Autowired
+    private JwtAuthenticationEntryPoint entryPoint;
+
+    @Bean
+    public AuthenticationManager authenticationManager() {//tra ve 1 doi tuong khi hop le,or tra ve exception khi ko hop le, or tra ve null khi ko the xu li
+        return new ProviderManager(Collections.singletonList(authenticationProvider));
+    }
+
+    @Bean
+    public JwtAuthenticationTokenFilter authenticationTokenFilter() {
+        JwtAuthenticationTokenFilter filter = new JwtAuthenticationTokenFilter();
+        filter.setAuthenticationManager(authenticationManager());
+        filter.setAuthenticationSuccessHandler(new JwtSuccessHandler());
+        return filter;
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("**/rest/**").authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(entryPoint)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(authenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.headers().cacheControl();
+
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("password").roles("ADMIN");
+       /*
+       // Mình comment phần dưới này vì chúng ta ko sử dụng DB nhé. Nếu các bạn sử dụng, bỏ comment và config query sao cho phù hợp. Các bạn có thể GG để tìm hiểu thêm
+       auth.jdbcAuthentication().dataSource(dataSource)
+               .usersByUsernameQuery("select username,password, enabled from users where username=?")
+               .authoritiesByUsernameQuery("select username, role from user_roles where username=?");
+       */
+    }
+}
